22a62978038ced55303241a026923960e1222cb0
parkhyeonggyu15 <gudrb15@naver.com>
2026-06-24 09:46:59 +0900

    06435

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log("HELLO WORLD!")
+
+//-------------------------------------
+//LODASH
+//-------------------------------------
+import _ from 'lodash'
+
+console.log(_);
+
+const result = _.join(['Hello','Parcel',],'|')
+console.log(result);
